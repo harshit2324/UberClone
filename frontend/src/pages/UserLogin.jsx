3f0ce3fd9414c05897f1ebf93fc2b8b4502e4af4
Harshit <richhariyaharshit684@gmail.com>
2025-01-03 11:42:07 +0530

    error: resolve login page error

--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -1,68 +1,93 @@
-import React, { useContext, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import  { UserDataContext } from '../context/UserContext'
-import axios from 'axios'
-
+import React, { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserDataContext } from "../context/UserContext";
+import axios from "axios";
 
 const UserLogin = () => {
-  const [email, setemail] = useState('')
-  const [password, setPassword] = useState('')
-  const [userData, setUserData] = useState('')
-  
- const {user, setUser} = useContext(UserDataContext)
- const Navigate = useNavigate()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const context = useContext(UserDataContext);
+  console.log(context)
+  const navigate = useNavigate();
 
   const submitHandler = async (e) => {
     e.preventDefault();
+
     const userData = {
       email: email,
-      password: password
-    }
+      password: password,
+    };
 
-    const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/users/login`, userData)
+    const response = await axios.post(
+      `${import.meta.env.VITE_BASE_URL}/users/login`,
+      userData
+    );
 
     if (response.status === 200) {
-      const data = response.data
-      setUser(data.user)
-      localStorage.setItem('token', data.token)
-      Navigate('/home')
+      const data = response.data;
+      setUser(data.user);
+      localStorage.setItem("token", data.token);
+      navigate("/home");
     }
-setemail('')
-setPassword('')
-  } 
-  return (
-    <div className='p-7 h-screen flex flex-col justify-between'>
-   <div>
-   <img className='w-16 mb-10 ' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="#" />
-<form onSubmit={(e) =>  submitHandler(e) }>
-  <h3 className=' text-lg font-medium mb-2'>what's your email</h3>
-  <input
-   required 
-   value={email}
-   onChange={(e) => {
-      setemail(e.target.value)
-   }}
-   className='bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base'
-   type="email"
-   placeholder='email@example.com' />
-  <h3 className=' text-lg font-medium mb-2'>Enter Password</h3>
-  <input 
-  required 
-  value={password}
-  onChange={(e) => {
-     setPassword(e.target.value)
-  }}
-  className='bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base' type="password" placeholder='password'/>
-  <button  className='bg-[#111] text-white font-semibold  mb-7 rounded px-4 py-2  w-full text-lg placeholder:text-base' >login</button>
- <p> New here? <Link to='/signup' className='text-blue-600'>Create new Account</Link></p>
-</form>
-   </div>
-  
-   <div>
- <Link to='/captain-login'  className='bg-[#10b461] text-white flex items-center justify-center font-semibold  mb-7 rounded px-4 py-2  w-full text-lg placeholder:text-base'>Sign in as Captain</Link>
-   </div>
-    </div>
-  )
-}
 
-export default UserLogin
+    setEmail("");
+    setPassword("");
+  };
+  return (
+    <div className="p-7 h-screen flex flex-col justify-between">
+      <div>
+        <img
+          className="w-16 mb-10 "
+          src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+          alt="#"
+        />
+        <form onSubmit={(e) => submitHandler(e)}>
+          <h3 className=" text-lg font-medium mb-2">what's your email</h3>
+          <input
+            required
+            value={email}
+            onChange={(e) => {
+              setemail(e.target.value);
+            }}
+            className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+            type="email"
+            placeholder="email@example.com"
+          />
+          <h3 className=" text-lg font-medium mb-2">Enter Password</h3>
+          <input
+            required
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+            type="password"
+            placeholder="password"
+          />
+          <button className="bg-[#111] text-white font-semibold  mb-7 rounded px-4 py-2  w-full text-lg placeholder:text-base">
+            login
+          </button>
+          <p>
+            {" "}
+            New here?{" "}
+            <Link to="/signup" className="text-blue-600">
+              Create new Account
+            </Link>
+          </p>
+        </form>
+      </div>
+
+      <div>
+        <Link
+          to="/captain-login"
+          className="bg-[#10b461] text-white flex items-center justify-center font-semibold  mb-7 rounded px-4 py-2  w-full text-lg placeholder:text-base"
+        >
+          Sign in as Captain
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default UserLogin;
